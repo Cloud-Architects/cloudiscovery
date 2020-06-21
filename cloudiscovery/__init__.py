cb2e17473cc9708a0dc14197fba52f8fc2759fca
Patryk Orwat <patrykorwat@gmail.com>
2020-06-21 18:46:58 +0700

    release 2.1.2

--- cloudiscovery/__init__.py
+++ cloudiscovery/__init__.py
@@ -45,7 +45,7 @@ if sys.version_info < (3, 6):
     print("Python 3.6 or newer is required", file=sys.stderr)
     sys.exit(1)
 
-__version__ = "2.1.0"
+__version__ = "2.1.2"
 
 AVAILABLE_LANGUAGES = ["en_US", "pt_BR"]
 DIAGRAMS_OPTIONS = ["True", "False"]
