af6a2e6f7b94d27c14f01c7a69212f85e3e49f77
Leandro Damascena <leandro.damascena@gmail.com>
2020-07-10 17:54:35 +0100

    Improved exception method #136

--- cloudiscovery/shared/error_handler.py
+++ cloudiscovery/shared/error_handler.py
@@ -1,16 +1,24 @@
 import functools
+import platform
+import traceback
+import sys
 
-from shared.common import log_critical
+import botocore
+import boto3
+
+from shared.common import log_critical, exit_critical
 
 
 # Decorator to catch exceptions and avoid stop script.
+
+
 def exception(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         # pylint: disable=broad-except
-        except Exception as e:
+        except botocore.client.ClientError as e:
             exception_str = str(e)
             if (
                 "Could not connect to the endpoint URL" in exception_str
@@ -24,5 +32,37 @@ def exception(func):
                     func.__qualname__, exception_str
                 )
             log_critical(message)
+
+        # pylint: disable=broad-except
+        except botocore.exceptions.UnknownServiceError:
+            log_critical("You're running a possible out of date boto3 version.")
+            log_critical("Please update boto3 to last version.")
+
+            issue_info = "\n".join(
+                (
+                    "Python:        {0}".format(sys.version),
+                    "boto3 version: {0}".format(boto3.__version__),
+                    "Platform:      {0}".format(platform.platform()),
+                    "",
+                    traceback.format_exc(),
+                )
+            )
+            exit_critical(issue_info)
+
+        # pylint: disable=broad-except
+        except Exception:  # pylint: disable=broad-except
+            log_critical("You've found a bug! Please, open an issue in GitHub project")
+            log_critical("https://github.com/Cloud-Architects/cloudiscovery/issues\n")
+
+            issue_info = "\n".join(
+                (
+                    "Python:        {0}".format(sys.version),
+                    "boto3 version: {0}".format(boto3.__version__),
+                    "Platform:      {0}".format(platform.platform()),
+                    "",
+                    traceback.format_exc(),
+                )
+            )
+            exit_critical(issue_info)
 
     return wrapper
