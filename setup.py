35037018d299199880c1b1215d58a63a6a125cef
Patryk Orwat <patrykorwat@gmail.com>
2020-10-25 22:30:37 +0800

    #97 switch to Python 3.8

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         "pytest-pythonpath",
     ],
     tests_suite="cloudiscovery",
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     scripts=["bin/cloudiscovery", "bin/cloudiscovery.cmd"],
     license="Apache License 2.0",
     classifiers=[
